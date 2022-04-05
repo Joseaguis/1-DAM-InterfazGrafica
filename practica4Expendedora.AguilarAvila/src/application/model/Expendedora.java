c6a2d22ef9cf33c12c159c3cd4ddf89d8a540534
Jose Manuel Aguilar <joseaguis1@hotmail.com>
2022-04-05 15:13:09 +0200

    New version

--- practica4Expendedora.AguilarAvila/src/application/model/Expendedora.java
+++ practica4Expendedora.AguilarAvila/src/application/model/Expendedora.java
@@ -26,23 +26,53 @@ public class Expendedora {
 	
 	public String toString() {
 		String texto = String.format(
-				  "Credito			: %6.2f euros"
-				+ "Cambio			: %6.2f euros"
-				+ "Importede ventas	: %6.2f euros"
+				  "Credito			: %6.2f euros%n"
+				+ "Cambio			: %6.2f euros%n"
+				+ "Importede ventas	: %6.2f euros%n"
 				+ "Productos: %d"
-				, this.credito,this.cambioDisponible,this.importeVentas);
+				, this.credito, this.cambioDisponible, this.importeVentas, this.nombreProductos.length);
 		
 		for (int i = 0; i < nombreProductos.length; i++) {
-			texto += String.format("%n%s	- %03.2f euros	- %d uds.",
+			texto += String.format("%n\t%s\t- %03.2f euros\t- %d uds.",
 					this.nombreProductos[i],this.precioProductos[i],this.stockProductos[i]);
 		}
 		return texto;
 	}
 	
-	public void anayadirDinero(double importe) {
-		if (this.credito + importe > ) {
-			
+	public void anyadirDinero(double importe) {
+		importe = importe * 100;
+		importe = Math.round(importe);
+		importe = importe / 100;
+		
+		if (this.credito + importe <= CREDITOMAXIMO) {
+			this.credito += importe;
 		}
+	}
+	
+	public double devolverCredito() {
+		double creditoDevolver = this.credito;
+		this.credito = 0;
+		return creditoDevolver;
+	}
+	
+	public double comprar(int i) throws StockInsuficienteException, CreditoInsuficienteException, CambioInsuficienteException {
+		String producto = this.nombreProductos[i];
+		double precio = this.precioProductos[i];
+		
+		if (this.stockProductos[i] < 1) throw new StockInsuficienteException("Stock insuficiente");
+		if (this.credito < precio) throw new CreditoInsuficienteException("Credito insuficiente");
+		if (this.cambioDisponible < (this.credito - precio)) throw new CambioInsuficienteException("Cambio insuficiente");
+		
+		stockProductos[i]--;
+		
+		double cambio = this.credito - precio;
+		
+		this.importeVentas += precio;
+		this.cambioDisponible -= cambio;
+		this.credito = 0;
+		
+		return cambio;
+		
 	}
 	
 	public double getCredito() {
